5b6b9836b817885ccf1a985d70f9fc9988aa4afb
Aero <aero.windwalker@gmail.com>
2026-06-04 21:40:54 +0800

    chore: bump version

--- Formula/tunnix.rb
+++ Formula/tunnix.rb
@@ -1,20 +1,20 @@
 class Tunnix < Formula
   desc "Encrypted SOCKS5/HTTP proxy tunnel over HTTP/SSE"
   homepage "https://github.com/aeroxy/tunnix"
-  version "0.2.3"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/aeroxy/tunnix/releases/download/0.2.3/tunnix_macos_arm64.zip"
-      sha256 "74d355fb8aeb0856d0df5054237df8b3af1957e3efa1c9e2ecb23ab95afbfaf2"
+      url "https://github.com/aeroxy/tunnix/releases/download/0.3.0/tunnix_macos_arm64.zip"
+      sha256 "fd02b6b89a610f38144b439579d94e72ef49658c7c671269fcf8ee62bc5de120"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/aeroxy/tunnix/releases/download/0.2.3/tunnix_linux_x86_64.zip"
-      sha256 "15f03fd737c5ffd5d16ed98adda0061d86ca313d4b3abb7d001cfa569a24daa8"
+      url "https://github.com/aeroxy/tunnix/releases/download/0.3.0/tunnix_linux_x86_64.zip"
+      sha256 "f002822f5836649962d845915cb1486ce8a6815a3653a49383313ecbfe1a1487"
     end
   end
 
