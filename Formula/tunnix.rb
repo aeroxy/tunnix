0a16978e622f4237da9adf04341527353b1e1123
Aero <aero.windwalker@gmail.com>
2026-07-10 00:47:40 +0800

    fix: update version to 0.4.1 in Cargo.toml, Cargo.lock, and tunnix.rb

--- Formula/tunnix.rb
+++ Formula/tunnix.rb
@@ -1,19 +1,19 @@
 class Tunnix < Formula
   desc "Encrypted SOCKS5/HTTP proxy tunnel over HTTP/SSE"
   homepage "https://github.com/aeroxy/tunnix"
-  version "0.4.0"
+  version "0.4.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/aeroxy/tunnix/releases/download/0.4.0/tunnix_macos_arm64.zip"
+      url "https://github.com/aeroxy/tunnix/releases/download/0.4.1/tunnix_macos_arm64.zip"
       sha256 "c4f1f61d1687a1c3b2ee67f31090fb1e0409a22b7de065d200cbc2320fae46ab"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/aeroxy/tunnix/releases/download/0.4.0/tunnix_linux_x86_64.zip"
+      url "https://github.com/aeroxy/tunnix/releases/download/0.4.1/tunnix_linux_x86_64.zip"
       sha256 "1afb23b7022b373db91d8120b39e5a5f17101d41535ad1da771951167e18705f"
     end
   end
